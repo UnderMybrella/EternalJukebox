5f02da15377420dd73dada67ccee80a1121608e7
Isaac <undermybrella@abimon.org>
2017-06-05 20:21:27 +1000

    Begun work on a new site function

--- src/main/kotlin/org/abimon/eternalJukebox/Experimental.kt
+++ src/main/kotlin/org/abimon/eternalJukebox/Experimental.kt
@@ -1,10 +1,11 @@
 package org.abimon.eternalJukebox
 
 import io.vertx.ext.web.Router
+import java.io.File
 
 object Experimental {
 
     fun setup(router: Router) {
-
+        router.get("/experimental/*").handler(StaticFileHandler("/experimental", File("experimental")))
     }
 }
