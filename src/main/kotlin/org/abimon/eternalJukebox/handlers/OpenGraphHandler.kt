0e23cc73c17b0ea02d9633e4504e6f898d070604
Isaac <undermybrella@abimon.org>
2017-07-24 14:26:08 +1000

    Update the search endpoint in the two search files

--- src/main/kotlin/org/abimon/eternalJukebox/handlers/OpenGraphHandler.kt
+++ src/main/kotlin/org/abimon/eternalJukebox/handlers/OpenGraphHandler.kt
@@ -1,9 +1,14 @@
 package org.abimon.eternalJukebox.handlers
 
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
 
 object OpenGraphHandler {
     fun setup(router: Router) {
+        router.get("/jukebox_go.html").handler(OpenGraphHandler::jukeboxOG)
+    }
+
+    fun jukeboxOG(context: RoutingContext) {
 
     }
 }
