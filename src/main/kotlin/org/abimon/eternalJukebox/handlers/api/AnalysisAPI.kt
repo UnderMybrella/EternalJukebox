6ef1b68b083d39611b8fe5be2ab92a540ec82e7b
Isaac <undermybrella@abimon.org>
2017-07-24 15:33:37 +1000

    Store the analysis rather than having to retrieve it every time

--- src/main/kotlin/org/abimon/eternalJukebox/handlers/api/AnalysisAPI.kt
+++ src/main/kotlin/org/abimon/eternalJukebox/handlers/api/AnalysisAPI.kt
@@ -3,11 +3,10 @@ package org.abimon.eternalJukebox.handlers.api
 import io.vertx.core.json.JsonArray
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
-import org.abimon.eternalJukebox.EternalJukebox
-import org.abimon.eternalJukebox.end
-import org.abimon.eternalJukebox.jsonObject
+import org.abimon.eternalJukebox.*
+import org.abimon.eternalJukebox.objects.EnumStorageType
 import org.abimon.eternalJukebox.objects.JukeboxInfo
-import org.abimon.eternalJukebox.toJsonObject
+import org.abimon.visi.io.ByteArrayDataSource
 
 object AnalysisAPI: IAPI {
     override val mountPath: String = "/analysis"
@@ -20,14 +19,30 @@ object AnalysisAPI: IAPI {
 
     fun analyseSpotify(context: RoutingContext) {
         val id = context.pathParam("id")
+        val update = context.request().getParam("update")?.toBoolean() ?: false
+        if(EternalJukebox.storage.isStored("$id.json", EnumStorageType.ANALYSIS) && !update) {
+            if(EternalJukebox.storage.provide("$id.json", EnumStorageType.ANALYSIS, context))
+                return
+
+            val data = EternalJukebox.storage.provide("$id.json", EnumStorageType.ANALYSIS)
+            if(data != null)
+                return context.response().end(data, "application/json")
+        }
+
+        if(update)
+            log("${context.request().connection().remoteAddress()} is requesting an update for $id")
+
         val track = EternalJukebox.spotify.analyse(id)
 
         if(track == null)
             context.response().setStatusCode(400).end(jsonObject {
                 put("error", "Track object is null")
             })
-        else
+        else {
             context.response().end(track.toJsonObject())
+
+            EternalJukebox.storage.store("$id.json", EnumStorageType.ANALYSIS, ByteArrayDataSource(track.toJsonObject().toString().toByteArray(Charsets.UTF_8)))
+        }
     }
 
     fun searchSpotify(context: RoutingContext) {
