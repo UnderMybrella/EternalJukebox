195fb4c3e873bf9f10b812ecaaaf345c342b2fd6
Isaac H <undermybrella@abimon.org>
2020-04-03 14:02:58 +1100

    Remove redundant Fuel classes

--- src/main/kotlin/org/abimon/eternalJukebox/FuelExtensions.kt
+++ src/main/kotlin/org/abimon/eternalJukebox/FuelExtensions.kt
@@ -1,12 +1,9 @@
 package org.abimon.eternalJukebox
 
 import com.github.kittinunf.fuel.core.Request
-import java.io.InputStream
-import java.io.OutputStream
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 import kotlin.reflect.full.declaredMemberProperties
-import kotlin.reflect.full.safeCast
 import kotlin.reflect.jvm.isAccessible
 
 //fun Request.source(data: InputStream, size: Long = data.available().toLong()): Request {
@@ -29,17 +26,11 @@ import kotlin.reflect.jvm.isAccessible
 //    return this
 //}
 
-val taskRequestClass = Class.forName("com.github.kittinunf.fuel.core.requests.TaskRequest")
-val taskRequestProperty = Request::class["taskRequest", taskRequestClass]
-
-val UPLOAD_TASK = Class.forName("com.github.kittinunf.fuel.core.requests.UploadTaskRequest").kotlin
-val progressCallbackProperty: KProperty<((Long, Long) -> Unit)?> = UPLOAD_TASK["progressCallback"]
-
-val Request.progressCallback: ((Long, Long) -> Unit)?
-    get() {
-        val task = UPLOAD_TASK.safeCast(taskRequestProperty.call(this)) ?: return null
-        return progressCallbackProperty.call(task)
-    }
+//val taskRequestClass = Class.forName("com.github.kittinunf.fuel.core.requests.TaskRequest")
+//val taskRequestProperty = Request::class["taskRequest", taskRequestClass]
+//
+//val UPLOAD_TASK = Class.forName("com.github.kittinunf.fuel.core.requests.UploadTaskRequest").kotlin
+//val progressCallbackProperty: KProperty<((Long, Long) -> Unit)?> = UPLOAD_TASK["progressCallback"]
 
 @Suppress("UNCHECKED_CAST", "UNUSED_PARAMETER")
 operator fun <C: Any, T> KClass<C>.get(property: String, returnClass: Class<T>): KProperty<T> {
@@ -56,16 +47,3 @@ operator fun <C: Any, T> KClass<C>.get(property: String): KProperty<T> {
 }
 
 fun Request.bearer(token: String): Request = header("Authorization" to "Bearer $token")
-
-fun InputStream.copyWithProgress(out: OutputStream, bufferSize: Int = DEFAULT_BUFFER_SIZE, progress: ((Long) -> Unit)?): Long {
-    var bytesCopied = 0L
-    val buffer = ByteArray(bufferSize)
-    var bytes = read(buffer)
-    while (bytes >= 0) {
-        out.write(buffer, 0, bytes)
-        bytesCopied += bytes
-        progress?.invoke(bytesCopied)
-        bytes = read(buffer)
-    }
-    return bytesCopied
-}
